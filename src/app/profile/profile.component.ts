8a951ec269b7276d51b86e1f2b028f88df84281f
karobia001 <karobiamaina81@gmail.com>
2019-10-01 08:03:21 +0300

    edit the ngOnit to resolve all classesand guarantee our bindings are readily available

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
-
+import { Repository } from '../repository';
+import { ProfileService } from '../profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,16 +13,28 @@ export class ProfileComponent implements OnInit {
   user: User;
    public profileSearch = 'karobia001';
    // public newUserName: string;
-   
+   repository: Repository;
+   reporesults: any;
+   userName: string;
 
 
-  constructor(  ) {
+
+  constructor( public profileService: ProfileService ) {
    }
-  
+   findProfile() {
+     this.profileService.updateProfile(this.userName);
+     this.profileService.getProfile();
+     this.user = this.profileService.user;
+     this.profileService.getProfileRepo(this.userName);
+     this.reporesults = this.profileService.otherRepo;
    }
 
 
   ngOnInit() {
-   
-  
+   this.profileService.getProfile();
+   this.user = this.profileService.user;
+   this.profileService.getProfileRepo(this.userName);
+   this.repository = this.profileService.repository;
+   console.log(this.profileService.repository.name);
+   // this.getRepositoryRequest.getProfileRepo(this.profileSearch);
 } }
